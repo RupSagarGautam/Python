8720669cc2fa939fa6fa25d99ec8dc32733c7d28
RupSagarGautam <rupsagargautam@gmail.com>
2025-06-16 17:08:30 +0545

    day 2 complete

---
+++ list.py
@@ -0,0 +1,13 @@
+# list
+
+list = ["Hello","World","Python","Programming",1,2,3, True,False,2.4]
+# print(list[:4])
+
+list.append("HelloWorld")
+# print(list)
+
+# for item in list:
+#     print(item,end=" ")
+    
+list.reverse()
+print(list,end=" ")
