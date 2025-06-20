b7e6c5ecc46bbe3872777cb4e610ae10eed2e1e0
RupSagarGautam <rupsagargautam@gmail.com>
2025-06-20 17:11:39 +0545

    Day 5 Completed

--- regex.py
+++ regex.py
@@ -2,13 +2,31 @@
 
 import re
 
-# pattern for regex
-pattern = r'[a-z]'
-string= "Test"
+# # pattern for regex
+# pattern = r'[a-z]'
+# string= "Test"
 
-# matching the pattern in the string
-searching_str = re.search(pattern,string)
-print(searching_str)
+# # searching the pattern in the string
+# if re.search(pattern,string):
+#     print("searching_str")
 
-matching_str = re.match(pattern,string)
-print(matching_str)
+# # matching the paggern in str
+# if re.match(pattern,string):
+#     print("matching_str")
+    
+# pattern = r'[A-Z]+[a-z]+\d'
+# string = input("Enter String")
+
+# if re.match(pattern,string):
+#     print("Pattern Matched")
+# else:
+#     print("Pattern not matched")
+    
+pattern = r'[A-Za-z0-9]/+@[A-Za-z]+/.com'
+string = input("Enter Your Email: ")
+
+if re.match(pattern,string):
+    print("Valid Email")
+
+else:
+    print("Invalid Email")
